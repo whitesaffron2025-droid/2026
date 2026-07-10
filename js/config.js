3797577a6a51ab83aaebf9243762823955fa8b0b
whitesaffron2025-droid <whitesaffron2025@gmail.com>
2026-07-10 08:08:47 +0500

    Configure Supabase project and 2026 table

--- js/config.js
+++ js/config.js
@@ -1,1 +1,9 @@
-window.CampaignConfig={};
+window.CampaignConfig = {
+  supabaseUrl: 'https://tkuivkhghmeljcuqwwdy.supabase.co',
+  supabaseKey: 'sb_publishable_pBOO7h7_TBtfDhv_vYqEFw_MCpHFkq-',
+  tableName: '2026',
+  batchSize: 1000,
+  pageSize: 20,
+  adminEmail: 'naappe@gmail.com',
+  adminName: 'Admin'
+};
